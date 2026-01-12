0413d518dc7ef9efdf0339f76c64f1fdee612b77
Washington own <canalABROBA@gmail.com>
2026-01-12 17:54:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3386287273-47f3f",
+  "appId": "1:16675939770:web:30baba78580672261b2983",
+  "apiKey": "AIzaSyBJsvWVIFt1s0WMNirTebziKKZZZ9vgZUo",
+  "authDomain": "studio-3386287273-47f3f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "16675939770"
+};
